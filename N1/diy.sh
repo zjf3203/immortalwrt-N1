8c1a7818b607a4cbf3b888cc2bf3024dc4dcdf6f
zjf3203 <110335302+zjf3203@users.noreply.github.com>
2025-03-04 20:18:41 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -17,11 +17,11 @@ echo "
 # 插件
 CONFIG_PACKAGE_luci-app-amlogic=y
 CONFIG_PACKAGE_luci-app-linkease=y
-CONFIG_PACKAGE_luci-app-lucky=y
+CONFIG_PACKAGE_luci-app-lucky=n
 " >> .config
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.6/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
